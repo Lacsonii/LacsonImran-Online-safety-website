b1dcae815894e82fc9233947fe3fe40794c26023
Lacsonii <152815763+Lacsonii@users.noreply.github.com>
2024-03-24 21:14:37 +0200

    added terms
    Added terms&conditions texts and image

--- termsAndConditions.php
+++ termsAndConditions.php
@@ -59,7 +59,94 @@
 <!-- === end navigation-bar text === --> 
 <div class="terms-bar">
 <img src = "images/lacson.png" alt = "Lacson Image" class = "lacson">
+<h1>Legal Notices</h1>
+</div>
+<div class="condition-bar">
+    <ul class="condition-links">
+        <li>Terms of Use</li>
+        <li>Copywrite</li>
+        <li>Privacy Policy</li>
+    </ul>
+</div>
+<div class="terms-paragraph">
+    <h1>Terms of Use</h1>
+        <p>
+            Welcome to the SMC Web sites (the "Site"). Through the Site, you have access to a variety of resources<br>
+            and content. These include: (a) software service offerings; (b) Web pages, data, messages, text, images,<br>
+            photographs, graphics, audios and videos (c) chat areas, bulletin boards, blogs, e-mail functions, and other <br>
+            services in connection with which you can upload, share, email, comment, review, transmit or<br>
+            otherwise access or make available Content (as defined below) ("Community Services"). Software, Materials,<br> 
+            Community Services, and other information, content and services are collectively referred to as "Content." <br>
+            The following are terms of a legal agreement between you ("You" or "Your") and SMC Corporation and its <br>
+            affiliated companies ("We" or "SMC"). By accessing or using the Site or the Content provided on or <br>
+            through the Site, you agree to follow and be bound by the following terms and conditions concerning <br>
+            your access to and use of the Site and the Content provided on or through the Site ("Terms of Use") <br>
+            and our Privacy Policy. SMC may revise the Terms of Use and Privacy Policy at any time without <br>
+            notice to you. The revised Terms of Use and Privacy Policy will be effective when posted. You can <br>
+            review the most current Terms of Use at <a href ="">www.smc-notworkinglink.com</a><br><br><br>
+        </p>
+    <h1>1. Terms Applicable to Specific Content and Areas of the Site</h1><br>
+        <p>
+            Certain parts of the website or content may have their own rules or terms in addition to these <br>
+            Terms of Use. If there's a conflict between these Terms of Use and those specific rules or terms for <br>
+            a particular part of the site or content, the specific rules or terms will apply for that part.<br><br><br>
+        </p>
+    <h1>2. Use of Software</h1>
+        <p>
+            When you use software provided on the site, you must follow the terms outlined in any accompanying license <br>
+            agreements or user agreements. If there are no specific license terms for the software, you may use it <br>
+            under these conditions:<br>
+            (a) You can only use the software for personal, noncommercial purposes.<br>
+            (b) You cannot modify or alter the software.<br>
+            (c) You cannot redistribute the software.<br><br><br>
+         </p>
+    <h1>3. Use of Materials</h1>
+        <p>
+            You're allowed to download, store, display, view, listen to, play, and print materials provided by SMC on the<br>
+            site, as long as you follow these rules:<br>
+            (a) Use the materials only for personal, noncommercial purposes.<br>
+            (b) Don't modify or alter the materials.<br>
+            (c) Don't redistribute the materials.<br><br><br>
+        </p>
+    <h1>4. Use of Community Services</h1>
+        <p>
+            SMC offers Community Services as a convenience, but they don't have to provide technical support or actively<br> 
+            participate. While these services might contain information about SMC products, they're not an official <br>
+            customer support channel.<br><br>
+            
+            When using Community Services, you agree to the following terms:<br>
+            (a) Use them only for personal, noncommercial purposes.<br>
+            (b) Don't redistribute or use the content in a way that violates these Terms of Use.<br>
+            (c) Don't store or collect personal data about other users unless expressly authorized by SMC.<br><br>
+        </p>
+    <h1>5. Termination of Use</h1>
+        <p>
+            SMC can stop or limit your access to the Site or its content at any time, based on its discretion.<br>
+            If SMC believes you've violated copyrights or laws, it can terminate your access. Oracle won't be <br>
+            liable for this action.<br><br>
+        </p>
 
+    <h1>6. Copyright/Trademark</h1>
+        <p>
+            Copyright © 1995, 2024, SMC and/or its affiliates.<br><br>
+
+            SMC®, is registered trademarks of SMC and/or its affiliates. Other names appearing on the Site may be <br>
+            trademarks of their respective owners.<br>
+
+            More information on the use of SMC trademarks. <br> <br>
+
+            Do not infringe on SMC's or others' intellectual property rights. Do not copy, distribute, modify, <br>
+            publicly display, or perform copyrighted works without permission. Do not remove or alter copyright <br>
+            notices from SMC software or any content on SMC's sites.<br><br><br>
+        </p>
+    <h1>7. Contact Information</h1>
+        <p>
+        If you have any questions regarding these Terms of Use, please contact Oracle at <a href="#"> sms_us@notworkinglink.com.</a> <br>
+        If you have questions about our Communities, please see our policies and contact information here. <br>
+        If you have any other questions, contact information is available at the Contact SMC page on the Site.<br><br>
+
+        Last Revised: March 24, 2024
+    </p>
 </div>
 <!-- === end Login pop-up text === -->
 <footer>
