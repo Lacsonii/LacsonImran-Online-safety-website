d783761f481768f60ad5393a9cd6cbf7bc338d52
Lacsonii <152815763+Lacsonii@users.noreply.github.com>
2024-03-30 14:00:36 +0200

    rearrangement of tags

--- Information.php
+++ Information.php
@@ -68,101 +68,132 @@
     <p class = "p-camapign">
         Be aware of the social media applications in this world, which we are sure you all know about them</br>
         This includes Facebook, Instagram and X-twitter (formerly know as twitter)</br>
-    </p></div>
-    <div class="parent-ing">
-        <div class="img-campaign">
-            <h2>ParentsLoveYou(PLY) Campaign</h2>
+    </p>
+</div>
+ <div class="parent-ing">
+    <div class="img-campaign">
+        <h2>ParentsLoveYou(PLY) Campaign</h2>
+        <img src = "images/parent-ing.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+        <h3>Campaign Aim</h3>
+        <p>
+            Empowering parents to engage in conversations about online safety with their teenagers,</br>
+            fostering trust and understanding to navigate the digital world together.
+        </p>
+        <h3>Vision</h3>
+        <p>
+            Creating a community where parents and teenagers collaborate to promote safer online behaviors and healthier</br>
+            digital habits, fostering a culture of trust and resilience in the digital age.
+        </p>
+        
+        <h3>Camapign Summary</h3>
+        <p>
+            The ParentsLoveYou (PLY) Campaign encourages parental involvement in discussing online safety </br>
+            with teenagers. By providing resources and support, it aims to foster trust and understanding within families, </br>
+            creating a safer online environment for adolescents.
+        </p>
+        <div class="img-c">
+            <h2>Let Me Browse</h2>
             <img src = "images/parent-ing.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+            <h3>Campaign Aim</h3>
+            <p>
+                Empower teens with knowledge and skills for safe online navigation, fostering digital citizenship </br>
+                and informed decision-making.
+            </p>
+            <h3>Vision</h3>
+            <p>
+                Create a digitally literate generation confident in utilizing the internet responsibly, promoting a culture</br>
+                of online safety and well-being through collaboration with schools, parents, and online platforms.</br>
+            </p>
+            <h3>Camapign Summary</h3>
+            <p>
+                "Let Me Browse" educates teens on online safety, empowering them to navigate the digital world responsibly.</br>
+                Through workshops and resources, it aims to equip teens with skills to identify and avoid online threats like cyberbullying and identity theft.</br>
+            </p>
+            <div class="img-c">
+                <h2>Get Better friend Online</h2>
+                <img src = "images/terrace.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
                 <h3>Campaign Aim</h3>
-                <p>Empowering parents to engage in conversations about online safety with their teenagers,</br>
-                    fostering trust and understanding to navigate the digital world together.
+                <p> 
+                    Empower teenagers with tools and knowledge for safer online interactions, reducing cyber <br>
+                    risks and fostering responsible digital citizenship.
                 </p>
                 <h3>Vision</h3>
                 <p>
-                    Creating a community where parents and teenagers collaborate to promote safer online behaviors and healthier</br>
-                    digital habits, fostering a culture of trust and resilience in the digital age.
+                    A digital landscape where teenagers navigate online spaces confidently, practicing empathy, respect, and digital <br>
+                    responsibility, contributing to a safer and more positive online community.<br>
                 </p>
-                
                 <h3>Camapign Summary</h3>
                 <p>
-                    The ParentsLoveYou (PLY) Campaign encourages parental involvement in discussing online safety </br>
-                    with teenagers. By providing resources and support, it aims to foster trust and understanding within families, </br>
-                    creating a safer online environment for adolescents.
+                    The "Get Better Friend Online" campaign aims to educate and empower teenagers<br>
+                    for safer online experiences. By addressing cyberbullying, privacy issues, and digital literacy,<br>
+                    it strives to create a secure and supportive online environment.
                 </p>
                 <div class="img-c">
-                    <h2>Let Me Browse</h2>
+                    <h2>Lets Be On Internet</h2>
                     <img src = "images/parent-ing.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+                    <h3>Campaign Aim</h3>
                     <p>
-                        <h3>Campaign Aim:</h3>&nbsp;Empower teens with knowledge and skills for safe online navigation, fostering digital citizenship </br>
-                        and informed decision-making.</br>
-                        <h3>Vision:&nbsp;</h3>Create a digitally literate generation confident in utilizing the internet responsibly, promoting a culture</br>
-                        of online safety and well-being through collaboration with schools, parents, and online platforms.</br>
-                    
-                        <h3>Camapign Summary:&nbsp;</h3>"Let Me Browse" educates teens on online safety, empowering them to navigate the digital world responsibly.</br>
-                        Through workshops and resources, it aims to equip teens with skills to identify and avoid online threats like cyberbullying and identity theft.</br>
+                        Equip teenagers with essential tools and awareness to safeguard themselves and<br>
+                        others online, reducing risks and promoting positive online behavior.
+                    </p>
+                    <h3>Vision</h3>
+                    <p>
+                        To cultivate a safer online space where teenagers can freely explore, learn, and connect, <br>
+                        ensuring a future where online safety is ingrained in digital culture.
+                    </p>
+                    <h3>Camapign Summary</h3>
+                    <p>
+                        "Let's Be On Internet" champions online safety for teenagers. Our mission is to empower <br>
+                        teens with the skills and knowledge to navigate the digital world safely, fostering a culture of <br>
+                        responsibility and respect online.
                     </p>
                     <div class="img-c">
-                        <h2>Get Better friend Online</h2>
-                        <img src = "images/terrace.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+                        <h2>WeAreTeenagers Campaign</h2>
+                        <img src = "images/we-are-teens.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+                        <h3>Campaign Aim</h3>
                         <p>
-                            <h3>Campaign Aim:</h3>Empower teenagers with tools and knowledge for safer online interactions, reducing cyber <br>
-                            risks and fostering responsible digital citizenship.<br>
-                            <h3>Vision:&nbsp;</h3>A digital landscape where teenagers navigate online spaces confidently, practicing empathy, respect, and digital <br>
-                            responsibility, contributing to a safer and more positive online community.<br>
-
-                            <h3>Camapign Summary:&nbsp;</h3>The "Get Better Friend Online" campaign aims to educate and empower teenagers<br>
-                            for safer online experiences. By addressing cyberbullying, privacy issues, and digital literacy,<br>
-                            it strives to create a secure and supportive online environment.<br>
+                            To raise awareness and equip teenagers with the knowledge and skills to protect<br> 
+                            themselves from online threats such as cyberbullying and exploitation.
+                        </p>
+                        <h3>Vision</h3>
+                        <p>
+                            A future where teenagers confidently explore the digital world, <br>
+                            expressing themselves creatively and connecting with others while prioritizing <br>
+                            their safety and well-being.
+                        </p>
+                        <h3>Camapign Summary</h3>
+                        <p>
+                            The WeAreTeenagers Campaign aims to promote online safety and digital literacy<br> 
+                            among teenagers. Through education and awareness initiatives, we empower teens to <br>
+                            navigate the digital world safely.
                         </p>
                         <div class="img-c">
-                            <h2>Lets Be On Internet</h2>
-                            <img src = "images/parent-ing.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+                            <h2>My Safety My Pride</h2>
+                            <img src = "images/woman-padlock.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
+                            <h3>Campaign Aim</h3>
                             <p>
-                                <h3>Campaign Aim:</h3>&nbsp;Equip teenagers with essential tools and awareness to safeguard themselves and<br>
-                                others online, reducing risks and promoting positive online behavior.<br>
-                                <h3>Vision:&nbsp;</h3>To cultivate a safer online space where teenagers can freely explore, learn, and connect, <br>
-                                ensuring a future where online safety is ingrained in digital culture.<br>
-                            
-                                <h3>Camapign Summary:&nbsp;</h3>"Let's Be On Internet" champions online safety for teenagers. Our mission is to empower <br>
-                                teens with the skills and knowledge to navigate the digital world safely, fostering a culture of <br>
-                                responsibility and respect online.<br>
+                                Empowering teens with skills and knowledge to navigate the online world safely, "My Safety My Pride" aims <br>
+                                to prevent cyber threats and foster digital citizenship among teenagers.
                             </p>
-                            <div class="img-c">
-                                <h2>WeAreTeenagers Campaign</h2>
-                                <img src = "images/we-are-teens.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
-                                <p>
-                                    <h3>Campaign Aim:&nbsp;</h3>To raise awareness and equip teenagers with the knowledge and skills to protect<br> 
-                                    themselves from online threats such as cyberbullying and exploitation.<br>
-                                    <h3>Vision:&nbsp;</h3>A future where teenagers confidently explore the digital world, <br>
-                                    expressing themselves creatively and connecting with others while prioritizing <br>
-                                    their safety and well-being.<br>
-                                    </br></br>
-                                    <h3>Camapign Summary:&nbsp;</h3>The WeAreTeenagers Campaign aims to promote online safety and digital literacy<br> 
-                                    among teenagers. Through education and awareness initiatives, we empower teens to <br>
-                                    navigate the digital world safely.<br>
-                                </p>
-                                <div class="img-c">
-                                    <h2>My Safety My Pride</h2>
-                                    <img src = "images/woman-padlock.jpg" name = "talk" alt = "talk-Image" class = "campaign-image">
-                                    <p>
-                                        <h3>Campaign Aim:&nbsp;</h3>Empowering teens with skills and knowledge to navigate the online world safely, "My Safety My Pride" aims <br>
-                                        to prevent cyber threats and foster digital citizenship among teenagers.<br>
-                                        <h3>Vision:&nbsp;</h3>Creating a generation of digitally savvy teens who prioritize online safety and ethical <br>
-                                        use of technology, "My Safety My Pride" envisions a future where teenagers confidently engage <br>
-                                        in online activities without fear, contributing to safer online communities.<br>
-                                        </br></br>
-                                        <h3>Camapign Summary:&nbsp;</h3>"My Safety My Pride" educates teens on online safety through <br>
-                                        workshops and multimedia. Aimed at preventing cyberbullying, identity theft, and online <br>
-                                        predators, it empowers teens to navigate the digital world responsibly.<br>
-                                    </p>
-                                </div>
-                            </div>
+                            <h3>Vision</h3>
+                            <p>
+                                Creating a generation of digitally savvy teens who prioritize online safety and ethical <br>
+                                use of technology, "My Safety My Pride" envisions a future where teenagers confidently engage <br>
+                                in online activities without fear, contributing to safer online communities.
+                            </p>
+                            <h3>Camapign Summary</h3>
+                            <p>
+                                "My Safety My Pride" educates teens on online safety through <br>
+                                workshops and multimedia. Aimed at preventing cyberbullying, identity theft, and online <br>
+                                predators, it empowers teens to navigate the digital world responsibly.<br>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 <footer>
 <div class="you-are-here">
