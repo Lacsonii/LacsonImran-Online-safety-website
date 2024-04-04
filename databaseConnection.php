8e0142223aa784644715a6c88c5bc73844cd2ab6
Lacsonii <152815763+Lacsonii@users.noreply.github.com>
2024-04-04 12:48:42 +0200

    Update databaseConnection.php
    added playstore Image

--- databaseConnection.php
+++ databaseConnection.php
@@ -24,21 +24,21 @@ $FirstName = $_POST["FirstName"];
 $LastName = $_POST["LastName"];
 $Username = $_POST["Username"];
 $Country = $_POST["Country"];
-$Email = $_POST["EmailAddress"];
+$EmailAddress = $_POST["EmailAddress"];
 $IDtype  = isset($_POST["IDtype"]) ? $_POST["IDtype"] : ""; // Use isset() to check if the key exists
-$Number = $_POST["IDNumber"];
+$IDNumber = $_POST["IDNumber"];
 $UploadImage = $_POST["UploadImage"];
 $Gender = $_POST["Gender"];
 $DateOfBirth = $_POST["DateOfBirth"];
 $password = $_POST["Password"];
 
 // Prepare and execute SQL query to insert into 'users' table
-$userQuery = "INSERT INTO webusers (Username, Password) VALUES ('$Username', '$password')";
+$userQuery = "INSERT INTO webusers VALUES ('$Username', '$password')";
 $retUsers = mysqli_query($conn, $userQuery);
 
 // Prepare and execute SQL query to insert into 'details' table
 if ($retUsers) {
-    $detailsQuery = "INSERT INTO useraccount (FirstName,LastName, Username,Country,EmailAddress, Gender,IDtype,IDNumber,UploadImage,DateOfBirth) VALUES ('$FirstName', '$LastName', '$Username','$Country','$Email','$Gender','$IDtype', '$Number','$UploadImage','$DateOfBirth')";
+    $detailsQuery = "INSERT INTO useraccount (FirstName,LastName, Username,Country,EmailAddress,IDtype,IDNumber,UploadImage, Gender, DateOfBirth) VALUES ('$FirstName', '$LastName', '$Username','$Country','$EmailAddress','$IDtype', '$IDNumber','$UploadImage','$Gender','$DateOfBirth')";
     $retDetails = mysqli_query($conn, $detailsQuery);
 
     if ($retDetails) {
