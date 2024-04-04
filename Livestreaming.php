f0c932068e6112c16e57dda5acf7a4a274dc9810
Lacsonii <152815763+Lacsonii@users.noreply.github.com>
2024-04-04 12:49:30 +0200

    Update Livestreaming.php
    added playstore Image

--- Livestreaming.php
+++ Livestreaming.php
@@ -224,6 +224,7 @@
                 <li><a href = "Livestreaming.php">Livestreaming</a></li>
                 <li><a href = "index.php">Home</a></li>
             </ul>
+            <img src ="images/playStore.png" alt ="PlayStore Badge" class="playstore">
         </div>
     <div class ="footer">
         <div class="icon">
