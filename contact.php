2d73a988be31b20991ae8746ad864cb8ae49e3ca
Lacsonii <152815763+Lacsonii@users.noreply.github.com>
2024-04-21 21:51:45 +0200

    Update contact.php
    head image

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@
     <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet" type="text/css"/>
+    <link rel="icon" href ="Images/smc.png" type="Images/smc">
+    <meta charset="utf-8">
     <title>Contact | SMC</title>
     <!-- Boxicons CSS -->
 <link href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel="stylesheet">
